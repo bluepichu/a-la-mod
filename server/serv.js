d72fc3455fca15f9ac15ded3c08cd6ca184d54e5
BluePichu <bluepichu64@gmail.com>
2015-01-30 19:46:08 -0500

    User authentication

--- server/serv.js
+++ server/serv.js
@@ -38,18 +38,18 @@ app.post("/user/new", function(req, res){
             res.send("Request failed: this email is associated with another account.");
             return;
         }
-        var salt = crypto.randomBytes(256).toString("base64");
-        var password = req.body.password;
 
-        for(var i = 0; i < HASH_COUNT; i++){
-            var hash = crypto.createHash("sha512");
-            password = hash.update(password).update(salt).digest("base64");
-        }
+        var salt = crypto.randomBytes(256).toString("base64");
+        var password = passwordHash(req.body.password, salt);
 
         db.insert("users", {
             email: req.body.email,
             password: password,
-            salt: salt
+            salt: salt,
+            authTokens: [],
+            chats: [],
+            icon: null,
+            screenName: req.body.email
         }, function(data, err){
             if(err){
                 // wtf
@@ -58,6 +58,46 @@ app.post("/user/new", function(req, res){
                 res.send("Ok.");
             }
         });
+    });
+});
+
+app.post("/user/auth", function(req, res){
+    if(!req.body.email){
+        res.send("Request failed: missing 'email' field.");
+        return;
+    }
+    if(!req.body.password){
+        res.send("Request failed: missing 'password' field.");
+        return;
+    }
+    db.query("users", {
+        email: req.body.email
+    },
+             function(data, err){
+        if(err){
+            res.send("Requst faild: server error.");
+            return;
+        }
+        if(data.length != 1){
+            res.send("Request failed: user not found.");
+            return;
+        }
+        if(passwordHash(req.body.password, data[0].salt) != data[0].password){
+            res.send("Request failed: incorrect password.");
+            return;
+        }
+
+        // issue auth token
+        var token = crypto.randomBytes(256).toString("base64");
+        res.send(token);
+
+        db.update("users", {
+            email: data[0].email
+        },
+                  {
+            $push: {authTokens: token}
+        },
+                  function(data, err){});
     });
 });
 
@@ -70,3 +110,11 @@ io.on("connection", function(socket){
 http.listen(1337, function(){
     console.log("listening on *:1337");
 });
+
+var passwordHash = function(password, salt){
+    for(var i = 0; i < HASH_COUNT; i++){
+        var hash = crypto.createHash("sha512");
+        password = hash.update(password).update(salt).digest("base64");
+    }
+    return password;
+}
