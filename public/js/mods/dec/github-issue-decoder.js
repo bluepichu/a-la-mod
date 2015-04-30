996f144ae0454596699fad4dda1c7bb2e4e08657
bluepichu <github@bluepichu.com>
2015-04-30 08:53:53 -0400

    Fixes #45

--- public/js/mods/dec/github-issue-decoder.js
+++ public/js/mods/dec/github-issue-decoder.js
@@ -3,8 +3,8 @@ function GithubIssueDecoder(){}
 GithubIssueDecoder.prototype.decode = function(inp, cb){
 	var out = inp.slice(0, inp.length);
 	for(var i = 0; i < inp.length; i++){
-		if(typeof(inp[i]) == "object" && inp[i].codec.type == "issue"){
-			out[i] = "<a href='" + inp[i].content.url + "' target='_blank'><i>#" + inp[i].content.number + ": " + inp[i].content.title + " [" + inp[i].content.state.toUpperCase() + "]</b></a>";
+		if(typeof(inp[i]) == "object" && inp[i].codec !== undefined && inp[i].codec.namespace == "com.alamod.github" && inp[i].codec.type == "issue"){
+			out[i] = new Handlebars.SafeString("<a href='" + inp[i].content.url + "' target='_blank'><i>#" + inp[i].content.number + ": " + inp[i].content.title + " [" + inp[i].content.state.toUpperCase() + "]</b></a>");
 		}
 	}
 	cb(out);
