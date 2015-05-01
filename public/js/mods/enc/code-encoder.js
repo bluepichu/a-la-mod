34ec9bf7b97bf4749caac0f72cf8c186fee23543
bluepichu <github@bluepichu.com>
2015-04-30 21:01:57 -0400

    Added Latex mods

---
+++ public/js/mods/enc/code-encoder.js
@@ -0,0 +1,38 @@
+function CodeEncoder(){
+	this.searchPattern = /`(.*?)`/;
+}
+
+CodeEncoder.prototype.encode = function(inp, cb){
+	var out = [];
+	for(var i = 0; i < inp.length; i++){
+		if(typeof(inp[i]) == "string"){
+			var str = inp[i];
+			while(str.length > 0){
+				var match = str.match(this.searchPattern);
+				if(match == null){
+					break;
+				}
+				if(match.index > 0){
+					out.push(str.substring(0, match.index));
+				}
+				out.push({
+					codec: {
+						namespace: "com.alamod",
+						type: "code"
+					},
+					content: {
+						code: match[1]
+					},
+					fallback: match[1]
+				});
+				str = str.substring(match.index + match[0].length);
+			}
+			if(str.length > 0){
+				out.push(str);
+			}
+		} else {
+			out.push(inp[i]);
+		}
+	}
+	cb(out);
+}
