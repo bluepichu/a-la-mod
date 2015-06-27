1d08e2e96a99f14fa41527dea333ed1f5ef060a0
Zach Wade <zacharywade@gmail.com>
2015-06-27 14:49:07 -0400

    i'm bad at subscripting

--- mods/creamery/links/enc/worker.js
+++ mods/creamery/links/enc/worker.js
@@ -1,8 +1,8 @@
 importScripts("/mods/utils/mod-base", "/mods/utils/pattern-matcher");
 
 registerMethod("encode", function(inp, cb){
-	matchPattern(inp.message, /([ "]|^)((https?:\/\/)?([\w_-]+\.)+\w+)(\(([\w ]+)\))?([ "]|$)/, function(match, cb){
-		console.log(match)
+	matchPattern(inp.message, /([ "]|^)((https?:\/\/)?([\w_-]+\.)+[\w\/\?=\&]+)(\(([\w ]+)\))?([ "]|$)/, function(match, cb){
+		console.log(match[2])
 		cb({
 			codec: {
 				namespace: "creamery",
@@ -12,7 +12,7 @@ registerMethod("encode", function(inp, cb){
 				url: match[3] ? match[2] : "http://" + match[2],
 				text: match[6] || match[2]
 			},
-			fallback: match[1]
+			fallback: match[2]
 		});
 	}, function(data){
 		cb({message: data});
